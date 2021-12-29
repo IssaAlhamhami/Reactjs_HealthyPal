71eff59d480e57b35561488ef371e10545f5d195
IssaAlhamhami <issaalhamhami91@gmail.com>
2021-12-29 22:59:17 +0400

    first version

---
+++ Issa Alhamhami ReactJs Project/src/shards-dashboard/scripts/main.js
@@ -0,0 +1,191 @@
+var users_json =JSON.parse('[{"id":1,"fullname":"Garrard","weight":92,"email":"gtwigley0@facebook.com","weightChanges":3,"meals":5,"photo":"https://randomuser.me/api/portraits/men/1.jpg","activities":[{"id":1,"value":48},{"id":2,"value":18},{"id":3,"value":38},{"id":4,"value":22},{"id":5,"value":18}]},{"id":2,"fullname":"Pansy","weight":81,"email":"pbess1@ovh.net","weightChanges":4,"meals":3,"photo":"https://randomuser.me/api/portraits/men/2.jpg","activities":[{"id":1,"value":39},{"id":2,"value":28}]},{"id":3,"fullname":"Francisca","weight":84,"email":"fmcmenamy2@wunderground.com","weightChanges":6,"meals":5,"photo":"https://randomuser.me/api/portraits/men/3.jpg","activities":[{"id":1,"value":16}]},{"id":4,"fullname":"Truda","weight":76,"email":"tbannell3@github.com","weightChanges":8,"meals":4,"photo":"https://randomuser.me/api/portraits/men/4.jpg","activities":[{"id":1,"value":38},{"id":2,"value":28}]},{"id":5,"fullname":"Hazel","weight":95,"email":"hjanuszewski4@edublogs.org","weightChanges":7,"meals":2,"photo":"https://randomuser.me/api/portraits/men/5.jpg","activities":[{"id":1,"value":35},{"id":2,"value":30},{"id":3,"value":29},{"id":4,"value":5}]},{"id":6,"fullname":"Eran","weight":93,"email":"eeisikowitch5@youtu.be","weightChanges":0,"meals":6,"photo":"https://randomuser.me/api/portraits/men/6.jpg","activities":[{"id":1,"value":16}]},{"id":7,"fullname":"Christina","weight":98,"email":"cklassmann6@wufoo.com","weightChanges":7,"meals":1,"photo":"https://randomuser.me/api/portraits/men/7.jpg","activities":[{"id":1,"value":7}]},{"id":8,"fullname":"Luce","weight":89,"email":"lingraham7@ca.gov","weightChanges":6,"meals":5,"photo":"https://randomuser.me/api/portraits/men/8.jpg","activities":[{"id":1,"value":11},{"id":2,"value":40},{"id":3,"value":14},{"id":4,"value":28},{"id":5,"value":27}]},{"id":9,"fullname":"Kellsie","weight":77,"email":"kpaulat8@engadget.com","weightChanges":2,"meals":2,"photo":"https://randomuser.me/api/portraits/men/9.jpg","activities":[{"id":1,"value":8},{"id":2,"value":23},{"id":3,"value":44}]},{"id":10,"fullname":"Sutton","weight":63,"email":"skemmis9@cyberchimps.com","weightChanges":9,"meals":5,"photo":"https://randomuser.me/api/portraits/men/10.jpg","activities":[{"id":1,"value":23}]},{"id":11,"fullname":"Raff","weight":79,"email":"rgoodbana@columbia.edu","weightChanges":1,"meals":6,"photo":"https://randomuser.me/api/portraits/men/11.jpg","activities":[{"id":1,"value":12},{"id":2,"value":28},{"id":3,"value":45},{"id":4,"value":5}]},{"id":12,"fullname":"Vannie","weight":81,"email":"vculmerb@mlb.com","weightChanges":5,"meals":5,"photo":"https://randomuser.me/api/portraits/men/12.jpg","activities":[{"id":1,"value":41}]},{"id":13,"fullname":"Cullie","weight":87,"email":"ctowllc@google.ca","weightChanges":2,"meals":1,"photo":"https://randomuser.me/api/portraits/men/13.jpg","activities":[{"id":1,"value":16}]},{"id":14,"fullname":"Gina","weight":81,"email":"gpasekd@fda.gov","weightChanges":4,"meals":4,"photo":"https://randomuser.me/api/portraits/men/14.jpg","activities":[{"id":1,"value":26},{"id":2,"value":42},{"id":3,"value":40}]},{"id":15,"fullname":"Kelbee","weight":65,"email":"kmasdinge@wunderground.com","weightChanges":6,"meals":4,"photo":"https://randomuser.me/api/portraits/men/15.jpg","activities":[{"id":1,"value":34},{"id":2,"value":42},{"id":3,"value":7}]},{"id":16,"fullname":"Andreana","weight":61,"email":"arubertisf@usatoday.com","weightChanges":1,"meals":4,"photo":"https://randomuser.me/api/portraits/men/16.jpg","activities":[{"id":1,"value":42},{"id":2,"value":45},{"id":3,"value":42}]},{"id":17,"fullname":"Jarid","weight":90,"email":"jclaytong@wufoo.com","weightChanges":0,"meals":1,"photo":"https://randomuser.me/api/portraits/men/17.jpg","activities":[{"id":1,"value":29},{"id":2,"value":36},{"id":3,"value":6},{"id":4,"value":8}]},{"id":18,"fullname":"Ernesta","weight":100,"email":"epiburnh@privacy.gov.au","weightChanges":2,"meals":7,"photo":"https://randomuser.me/api/portraits/men/18.jpg","activities":[{"id":1,"value":7},{"id":2,"value":21},{"id":3,"value":30}]},{"id":19,"fullname":"Izak","weight":92,"email":"ibreazealli@people.com.cn","weightChanges":5,"meals":5,"photo":"https://randomuser.me/api/portraits/men/19.jpg","activities":[{"id":1,"value":29},{"id":2,"value":49},{"id":3,"value":32},{"id":4,"value":5},{"id":5,"value":35}]},{"id":20,"fullname":"Tamra","weight":94,"email":"tstroderj@paginegialle.it","weightChanges":1,"meals":6,"photo":"https://randomuser.me/api/portraits/men/20.jpg","activities":[{"id":1,"value":20},{"id":2,"value":23},{"id":3,"value":24}]},{"id":21,"fullname":"Lisle","weight":60,"email":"lhornungk@aboutads.info","weightChanges":5,"meals":5,"photo":"https://randomuser.me/api/portraits/men/21.jpg","activities":[{"id":1,"value":14}]},{"id":22,"fullname":"Haywood","weight":99,"email":"hmcroriel@naver.com","weightChanges":1,"meals":6,"photo":"https://randomuser.me/api/portraits/men/22.jpg","activities":[{"id":1,"value":17},{"id":2,"value":9},{"id":3,"value":44}]},{"id":23,"fullname":"Jacinthe","weight":96,"email":"janningm@hubpages.com","weightChanges":8,"meals":5,"photo":"https://randomuser.me/api/portraits/men/23.jpg","activities":[{"id":1,"value":23},{"id":2,"value":15},{"id":3,"value":41}]},{"id":24,"fullname":"Libbi","weight":77,"email":"lcolinn@mlb.com","weightChanges":6,"meals":1,"photo":"https://randomuser.me/api/portraits/men/24.jpg","activities":[{"id":1,"value":45}]},{"id":25,"fullname":"Brinn","weight":84,"email":"bbehneo@nationalgeographic.com","weightChanges":8,"meals":5,"photo":"https://randomuser.me/api/portraits/men/25.jpg","activities":[{"id":1,"value":15}]},{"id":26,"fullname":"Sallee","weight":87,"email":"ssmallcombp@etsy.com","weightChanges":7,"meals":2,"photo":"https://randomuser.me/api/portraits/men/26.jpg","activities":[{"id":1,"value":18}]},{"id":27,"fullname":"Emile","weight":89,"email":"edecourtneyq@ed.gov","weightChanges":5,"meals":2,"photo":"https://randomuser.me/api/portraits/men/27.jpg","activities":[{"id":1,"value":18},{"id":2,"value":11},{"id":3,"value":47}]},{"id":28,"fullname":"Bruce","weight":79,"email":"bblucherr@rambler.ru","weightChanges":4,"meals":2,"photo":"https://randomuser.me/api/portraits/men/28.jpg","activities":[{"id":1,"value":30},{"id":2,"value":29},{"id":3,"value":13},{"id":4,"value":41},{"id":5,"value":9}]},{"id":29,"fullname":"Rochester","weight":94,"email":"rdelacours@examiner.com","weightChanges":9,"meals":3,"photo":"https://randomuser.me/api/portraits/men/29.jpg","activities":[{"id":1,"value":30},{"id":2,"value":24},{"id":3,"value":6}]},{"id":30,"fullname":"Clyve","weight":64,"email":"cstathert@w3.org","weightChanges":6,"meals":1,"photo":"https://randomuser.me/api/portraits/men/30.jpg","activities":[{"id":1,"value":13},{"id":2,"value":6},{"id":3,"value":38},{"id":4,"value":19}]},{"id":31,"fullname":"Babbette","weight":93,"email":"bsnalhamu@t-online.de","weightChanges":2,"meals":6,"photo":"https://randomuser.me/api/portraits/men/31.jpg","activities":[{"id":1,"value":6},{"id":2,"value":6},{"id":3,"value":45}]},{"id":32,"fullname":"Kelcey","weight":79,"email":"ktrinemanv@unicef.org","weightChanges":1,"meals":1,"photo":"https://randomuser.me/api/portraits/men/32.jpg","activities":[{"id":1,"value":25},{"id":2,"value":40},{"id":3,"value":7},{"id":4,"value":23},{"id":5,"value":27}]},{"id":33,"fullname":"Kendrick","weight":78,"email":"kantoszewskiw@webnode.com","weightChanges":0,"meals":7,"photo":"https://randomuser.me/api/portraits/men/33.jpg","activities":[{"id":1,"value":14},{"id":2,"value":35},{"id":3,"value":19}]},{"id":34,"fullname":"Kipper","weight":74,"email":"kmontelx@google.com.hk","weightChanges":1,"meals":4,"photo":"https://randomuser.me/api/portraits/men/34.jpg","activities":[{"id":1,"value":39},{"id":2,"value":13},{"id":3,"value":17}]},{"id":35,"fullname":"Alvis","weight":84,"email":"agricewoody@unicef.org","weightChanges":0,"meals":4,"photo":"https://randomuser.me/api/portraits/men/35.jpg","activities":[{"id":1,"value":20},{"id":2,"value":26},{"id":3,"value":41},{"id":4,"value":27}]},{"id":36,"fullname":"Griffith","weight":66,"email":"gwolseleyz@jigsy.com","weightChanges":8,"meals":5,"photo":"https://randomuser.me/api/portraits/men/36.jpg","activities":[{"id":1,"value":50},{"id":2,"value":12},{"id":3,"value":43},{"id":4,"value":8}]},{"id":37,"fullname":"Hartley","weight":77,"email":"halabaster10@blog.com","weightChanges":9,"meals":4,"photo":"https://randomuser.me/api/portraits/men/37.jpg","activities":[{"id":1,"value":27},{"id":2,"value":20},{"id":3,"value":40}]},{"id":38,"fullname":"Jessi","weight":86,"email":"jfinnemore11@nasa.gov","weightChanges":3,"meals":7,"photo":"https://randomuser.me/api/portraits/men/38.jpg","activities":[{"id":1,"value":12},{"id":2,"value":49},{"id":3,"value":33}]},{"id":39,"fullname":"Devora","weight":88,"email":"dbillingsley12@jigsy.com","weightChanges":0,"meals":1,"photo":"https://randomuser.me/api/portraits/men/39.jpg","activities":[{"id":1,"value":32},{"id":2,"value":40},{"id":3,"value":7}]},{"id":40,"fullname":"Carla","weight":93,"email":"ckahane13@cdbaby.com","weightChanges":1,"meals":4,"photo":"https://randomuser.me/api/portraits/men/40.jpg","activities":[{"id":1,"value":38},{"id":2,"value":21}]},{"id":41,"fullname":"Jeanne","weight":79,"email":"jbaud14@vistaprint.com","weightChanges":10,"meals":3,"photo":"https://randomuser.me/api/portraits/men/41.jpg","activities":[{"id":1,"value":47},{"id":2,"value":10}]},{"id":42,"fullname":"Bellanca","weight":83,"email":"bdiboldi15@skyrock.com","weightChanges":1,"meals":3,"photo":"https://randomuser.me/api/portraits/men/42.jpg","activities":[{"id":1,"value":25},{"id":2,"value":39},{"id":3,"value":44}]},{"id":43,"fullname":"Lisha","weight":83,"email":"lharce16@purevolume.com","weightChanges":6,"meals":6,"photo":"https://randomuser.me/api/portraits/men/43.jpg","activities":[{"id":1,"value":41},{"id":2,"value":33},{"id":3,"value":34}]},{"id":44,"fullname":"Bear","weight":93,"email":"bjoules17@nature.com","weightChanges":4,"meals":2,"photo":"https://randomuser.me/api/portraits/men/44.jpg","activities":[{"id":1,"value":17},{"id":2,"value":21},{"id":3,"value":47},{"id":4,"value":13},{"id":5,"value":6}]},{"id":45,"fullname":"Katheryn","weight":98,"email":"kpack18@nyu.edu","weightChanges":3,"meals":2,"photo":"https://randomuser.me/api/portraits/men/45.jpg","activities":[{"id":1,"value":29},{"id":2,"value":7},{"id":3,"value":49},{"id":4,"value":10}]},{"id":46,"fullname":"Dud","weight":81,"email":"dgot19@ow.ly","weightChanges":8,"meals":2,"photo":"https://randomuser.me/api/portraits/men/46.jpg","activities":[{"id":1,"value":40},{"id":2,"value":46},{"id":3,"value":37},{"id":4,"value":37}]},{"id":47,"fullname":"Mac","weight":88,"email":"mlattimore1a@businessinsider.com","weightChanges":6,"meals":3,"photo":"https://randomuser.me/api/portraits/men/47.jpg","activities":[{"id":1,"value":10},{"id":2,"value":22},{"id":3,"value":35},{"id":4,"value":25}]},{"id":48,"fullname":"Marcos","weight":79,"email":"mvivers1b@biglobe.ne.jp","weightChanges":9,"meals":2,"photo":"https://randomuser.me/api/portraits/men/48.jpg","activities":[{"id":1,"value":41}]},{"id":49,"fullname":"Jon","weight":96,"email":"jgiffon1c@g.co","weightChanges":7,"meals":1,"photo":"https://randomuser.me/api/portraits/men/49.jpg","activities":[{"id":1,"value":13},{"id":2,"value":13},{"id":3,"value":31},{"id":4,"value":34}]},{"id":50,"fullname":"Timotheus","weight":87,"email":"thaug1d@cnet.com","weightChanges":10,"meals":4,"photo":"https://randomuser.me/api/portraits/men/50.jpg","activities":[{"id":1,"value":28},{"id":2,"value":36},{"id":3,"value":37},{"id":4,"value":21}]}]');
+var html = '';
+//$.getScript('scripts/chart.js')
+var boSmallStatsDatasets_new = [];
+for (var i = 0; i < users_json.length; i++) {
+  var index = i + 1;
+  var data1 = [];
+  for (var ii = 0; ii < users_json[i].activities.length; ii++) {
+    data1.push(users_json[i].activities[ii].value);
+  }
+  boSmallStatsDatasets_new.push({ backgroundColor: 'rgba(0, 184, 216, 0.1)', borderColor: 'rgb(0, 184, 216)', data: data1 });
+
+  html += '<div class="col-lg-4 col-md-6 col-sm-6 mb-4 ">';
+  html += '  <div class="stats-small stats-small--1 card card-small ">';
+  html += '    <div class="card-header border-bottom text-center">';
+  html += '      <div class="mb-3 mx-auto">';
+  html += '        <img class="rounded-circle" src="' + users_json[i].photo +'" alt="User Avatar" width="110">';
+  html += '                </div>';
+  html += '        <h4 class="mb-0">'+users_json[i].fullname+'</h4>';
+  html += '        <span class="text-muted d-block mb-2">' + users_json[i].email +'</span>';
+  html += '        <span class="text-warning mb-2">weight :' + users_json[i].weight + ' Kg</span>';
+  html += '      </div>';
+  html += '      <div class="card-body m-2 row">';
+  html += '          <table class="table table-bordered col-lg-12">';
+  html += '            <tbody>';
+  html += '              <tr class="row">';
+  html += '                <td class="col-lg-6 text-center"><p class="col-lg-12">Meals </p> <span class="col-lg-12 text-primary text-center mb-2">' + users_json[i].meals + '</span></td>';
+  html += '                <td class="col-lg-6 text-center"><p class="col-lg-12">Lost Weight </p> <span class="col-lg-12 text-warning text-center mb-2">' + users_json[i].weightChanges + ' kg</span></td>';
+  html += '              </tr>';
+  html += '            </tbody>';
+  html += '          </table>';
+  html += '        <div class="row col-lg-12">';
+  html += '          <canvas height="120" class="blog-overview-stats-small-' + index + '"></canvas>';
+  html += '        </div>';
+  html += '      <div class="row col-lg-12">';
+
+  html += '   <button type="button" class="col-lg-5  mr-2 btn btn-sm btn-outline-primary mr-1">Message</button>';
+  html += '   <button type="button" class="col-lg-5  ml-4 btn btn-sm btn-outline-dark mr-1">Profile</button>';
+  html += '        </div>';
+  html += '        </div>';
+
+
+  html += '    </div>';
+  html += '  </div>';
+
+
+
+}
+console.log(JSON.stringify( boSmallStatsDatasets_new));
+  $(".users_div").append(html);
+
+
+
+  (function ($) {
+    $(document).ready(function () {
+
+      // Blog overview date range init.
+      $('#blog-overview-date-range').datepicker({});
+
+      //
+      // Small Stats
+      //
+      // Datasets
+      var boSmallStatsDatasets = boSmallStatsDatasets_new;
+
+      // Options
+      function boSmallStatsOptions(max) {
+        return {
+          maintainAspectRatio: true,
+          responsive: true,
+          // Uncomment the following line in order to disable the animations.
+          // animation: false,
+          legend: {
+            display: false
+          },
+          tooltips: {
+            enabled: false,
+            custom: false
+          },
+          elements: {
+            point: {
+              radius: 0
+            },
+            line: {
+              tension: 0.3
+            }
+          },
+          scales: {
+            xAxes: [{
+              gridLines: false,
+              scaleLabel: false,
+              ticks: {
+                display: false
+              }
+            }],
+            yAxes: [{
+              gridLines: false,
+              scaleLabel: false,
+              ticks: {
+                display: false,
+                // Avoid getting the graph line cut of at the top of the canvas.
+                // Chart.js bug link: https://github.com/chartjs/Chart.js/issues/4790
+                suggestedMax: max
+              }
+            }],
+          },
+        };
+      }
+
+      // Generate the small charts
+      boSmallStatsDatasets.map(function (el, index) {
+        var chartOptions = boSmallStatsOptions(Math.max.apply(Math, el.data) + 1);
+        var ctx = document.getElementsByClassName('blog-overview-stats-small-' + (index + 1));
+        new Chart(ctx, {
+          type: 'line',
+          data: {
+            labels: ["Label 1", "Label 2", "Label 3", "Label 4", "Label 5"],
+            datasets: [{
+              label: 'Today',
+              fill: 'start',
+              data: el.data,
+              backgroundColor: el.backgroundColor,
+              borderColor: el.borderColor,
+              borderWidth: 1.5,
+            }]
+          },
+          options: chartOptions
+        });
+      });
+
+
+      
+
+      // Hide initially the first and last analytics overview chart points.
+      // They can still be triggered on hover.
+      var aocMeta = BlogOverviewUsers.getDatasetMeta(0);
+      aocMeta.data[0]._model.radius = 0;
+      aocMeta.data[bouData.datasets[0].data.length - 1]._model.radius = 0;
+
+      // Render the chart.
+      window.BlogOverviewUsers.render();
+
+      //
+      // Users by device pie chart
+      //
+
+      // Data
+      var ubdData = {
+        datasets: [{
+          hoverBorderColor: '#ffffff',
+          data: [68.3, 24.2, 7.5],
+          backgroundColor: [
+            'rgba(0,123,255,0.9)',
+            'rgba(0,123,255,0.5)',
+            'rgba(0,123,255,0.3)'
+          ]
+        }],
+        labels: ["Desktop", "Tablet", "Mobile"]
+      };
+
+      // Options
+      var ubdOptions = {
+        legend: {
+          position: 'bottom',
+          labels: {
+            padding: 25,
+            boxWidth: 20
+          }
+        },
+        cutoutPercentage: 0,
+        // Uncomment the following line in order to disable the animations.
+        // animation: false,
+        tooltips: {
+          custom: false,
+          mode: 'index',
+          position: 'nearest'
+        }
+      };
+
+      var ubdCtx = document.getElementsByClassName('blog-users-by-device')[0];
+
+      // Generate the users by device chart.
+      window.ubdChart = new Chart(ubdCtx, {
+        type: 'pie',
+        data: ubdData,
+        options: ubdOptions
+      });
+
+    });
+  })(jQuery);
+
